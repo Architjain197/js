ad4a2be7df4f2504c6e0090b1721ac2d5b5738f5
Architjain197 <architjn19702@gmail.com>
2026-01-30 20:16:32 +0000

    add initial variable declarations and logging

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 1445533
+let accountEmail = "archit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 
+
+console.log(accountId)
+
+
+accountEmail="adi@gmail.com"
+accountPassword="2323232323"
+accountCity="delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
