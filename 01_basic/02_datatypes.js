d69bd2f49a0bfc67165043f2351d3a284a0fa0ce
Architjain197 <architjn19702@gmail.com>
2026-01-31 06:58:30 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version 
+
+// alert("Hello") we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Archit")  // code readability should be high 
+
+let Name ="Archit"
+let age= 23
+let isLoggedIn = false 
+let state=null 
+
+console.table([Name,age,isLoggedIn,state])
+
+//number => 2 to power 53
+//bigint (basically use in stocks and very hug number)
+
+//null => standalone value
+
+//undefined => 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object 
