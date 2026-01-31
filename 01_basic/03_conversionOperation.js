ddc2c9a3d0c3785905fa7b1f939a62ac5a5c1f38
Architjain197 <architjn19702@gmail.com>
2026-01-31 07:20:45 +0000

    Conversion_of_datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = true
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+let isLoggedIn = "Archit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+let someNumber = 19
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
