19bae7e38ab52744a1213509e1dbab0cab5f27db
Architjain197 <architjn19702@gmail.com>
2026-01-31 10:01:46 +0000

    conversion_of_datatype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = true
 
 console.log(typeof score)
@@ -20,3 +21,24 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+*/
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1="hitesh"
+let str2=" Jain"
+
+let str3=str1+str2
+console.log(str3)
+
+let num1,num2,num3
+num1=num2=num3= 2 + 2
+
+console.log(num1+num2)
+
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter)
