a8ba3b10045d94cb372b2f1d22b4ad3660213372
Architjain197 <architjn19702@gmail.com>
2026-02-02 08:20:02 +0000

    string

---
+++ 01_basic/04_string.js
@@ -0,0 +1,37 @@
+const FirstName="Archit"
+const LastName=" Jain"
+
+// console.log(FirstName+LastName);
+
+// String Polation by using back tags 
+
+console.log(`first name ${FirstName} and last name is ${LastName}`)
+
+const gameName = new String('Cyclerun')
+
+console.log(gameName[0])
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf('r'))
+
+// how to devide it into sub string 
+const newstring = gameName.substring(0,4)
+console.log(newstring)
+// slice in strings forward and reverse order 
+const anotherstring = gameName.slice(-7,4)
+console.log(anotherstring);
+
+// use of trim (trim is basically use for removeing the extraspace form the word to protact our database from unusal spaces )
+const newstringone="   Aadi    "
+console.log(newstringone)
+console.log(newstringone.trim());
+
+const url="https://archit.com/hitesh%20jain"
+console.log(url)
+console.log(url.replace('%20','-'));
+
+
+
+
